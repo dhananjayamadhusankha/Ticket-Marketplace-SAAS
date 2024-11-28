66241c3b88a46ced31ef37dba927d80fae197897
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-11-28 14:24:09 +0530

    header is completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,8 +31,49 @@ function Header() {
             </SignedOut>
           </div>
         </div>
-        <div className="w-full lg:w-auto">
+        <div className="w-full lg:max-w-2xl">
           <SearchBar />
+        </div>
+
+        <div className="hidden lg:block lg:ml-auto">
+          <SignedIn>
+            <div className="flex items-center gap-3">
+              <Link href="/seller">
+                <button className="bg-blue-600 text-white px-3 py-1.5 rounded-lg text-sm hover-bg-blue-700 transition">
+                  Sell Tickets
+                </button>
+              </Link>
+              <Link href="/tickets">
+                <button className="bg-gray-100 text-gray-800 px-3 py-1.5 rounded-lg text-sm hover-bg-gray-200 transition border border-gray-300">
+                  My Tickets
+                </button>
+              </Link>
+              <UserButton />
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="bg-gray-100 text-gray-800 px-3 py-1.5 rounded-lg text-sm hover:bg-gray-200 border border-gray-300 transition">
+                Sign in
+              </button>
+            </SignInButton>
+          </SignedOut>
+        </div>
+
+        <div className="lg:hidden w-full flex justify-center items-center gap-3">
+          <SignedIn>
+              <Link href="/seller" className="flex-1">
+                <button className="bg-blue-600 text-white w-full px-3 py-1.5 rounded-lg text-sm hover-bg-blue-700 transition">
+                  Sell Tickets
+                </button>
+              </Link>
+              <Link href="/tickets" className="flex-1">
+                <button className="bg-gray-100 text-gray-800 w-full px-3 py-1.5 rounded-lg text-sm hover-bg-gray-200 transition border border-gray-300">
+                  My Tickets
+                </button>
+              </Link>
+          </SignedIn>
+          
         </div>
       </div>
     </header>
